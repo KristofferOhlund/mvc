eda3ca17ac06bb798de8c02075ecf04e1712d4cf
Kristoffer <kristoffer.ohlund@icloud.com>
2025-04-23 14:55:35 +0200

    New Route, Game, first commit

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -2,19 +2,59 @@
 
 namespace App\Controller;
 
+use PDepend\Metrics\Analyzer\CodeRankAnalyzer\MethodStrategy;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Card\DeckOfCards;
+use App\Card\CardHand;
+use App\Game\Player;
+use App\Game\Bank;
+
 class GameController extends AbstractController {
+    public function createSession(SessionInterface $session) {
+        
+        $session->set("player", new Player("Kingen"));
+        $session->set("player", new Bank("Bank"));
+        $session->set("deckOfCards", new DeckOfCards());
+
+        // $session["bank"] = new Bank();
+    }
+
+    #[Route("/game/destroy", name:"game_destroy")]
+    public function gameDestroy(SessionInterface $session) {
+        $session->set("player", null);
+        $session->set("bank", null);
+        $session->set("deckOfCards", null);
+
+        return $this->redirectToRoute(("game_play"));
+    }
+
     #[Route("/game", name:"game_info")]
     public function game(): Response {
         return $this->render("game/game-info.html.twig");
     }
 
-    #[Route("/game/play", name:"game_play")]
-    public function gamePlay(): Response {
-        return $this->render("game/game-play.html.twig");
+    #[Route("/game/play", name:"game_play", methods:["GET"])]
+    public function gamePlay(SessionInterface $session): Response {
+        
+        $player = $session->get("player") ? null : $this->createSession($session);
+        $player = $session->get("player");
+
+        $data = [
+            "player" => $player
+        ];
+
+        return $this->render("game/game-play.html.twig", $data);
+    }
+
+    #[Route("/game/play", name:"game_roll", methods:["POST"])]
+    public function gameRoll(SessionInterface $session): Response {
+
+
+        return $this->redirectToRoute(("game_play"));
     }
 
     #[Route("/game/doc", name:"game_doc")]
---
+++ src/Game/Bank.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Player class, representing a player in Card game
+ */
+
+namespace App\Game;
+
+use App\Card\CardGraphic;
+
+class Bank extends Player {
+
+}
