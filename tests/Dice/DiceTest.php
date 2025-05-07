4da9dbec533b7368e8408fed9a26ee0d918359c4
Kristoffer <kristoffer.ohlund@icloud.com>
2025-05-07 11:47:20 +0200

    PHPunit test practise

---
+++ tests/Dice/DiceTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Dice;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test cases for class Dice.
+ */
+class DiceTest extends TestCase
+{
+    /**
+     * Construct object and verify that the object has the expected
+     * properties, use no arguments.
+     */
+    public function testCreateDice()
+    {
+        $die = new Dice();
+        $this->assertInstanceOf("\App\Dice\Dice", $die);
+
+        $res = $die->getAsString();
+        $this->assertNotEmpty($res);
+    }
+
+    public function testRoll()
+    {
+        $die = new Dice();
+        $roll = $die->roll();
+        $exp = $die->getValue();
+
+        $this->assertEquals($exp, $roll);
+    }
+}
