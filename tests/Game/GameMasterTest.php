bbcff26e5d60dbfbeafc28c43449e6e0fbb156db
Kristoffer <kristoffer.ohlund@icloud.com>
2025-05-08 10:31:42 +0200

    PHpstan fix

--- tests/Game/GameMasterTest.php
+++ tests/Game/GameMasterTest.php
@@ -17,7 +17,7 @@ class GameMasterTest extends TestCase
      * Create object with Player arguments
      */
     public function testCreateGameMaster()
-    {   
+    {
         /** @var Player&\PHPunit\Framework\MockObject\MockObject $player */
         $player = $this->createMock(Player::class);
 
@@ -54,20 +54,21 @@ class GameMasterTest extends TestCase
         $player = $this->createMock(Player::class);
         /** @var Player&\PHPunit\Framework\MockObject\MockObject $player1*/
         $player1 = $this->createMock(Player::class);
-        
+
         $player->method("getPoints")->willReturn(22);
         $player1->method("getPoints")->willReturn(10);
 
         $gameMaster = new GameMaster($player);
         $gameMaster->addPlayer($player1);
-        
 
         $this->assertIsBool($gameMaster->checkPlayersDone());
         $this->assertIsArray($gameMaster->declareWinner());
 
         $gameMaster->enqueue($player1);
 
-        $this->assertInstanceOf("App\Game\Player", 
-        $gameMaster->getClosestPlayerTest());
+        $this->assertInstanceOf(
+            "App\Game\Player",
+            $gameMaster->getClosestPlayerTest()
+        );
     }
 }
