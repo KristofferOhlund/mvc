8d948dafe27942b52aa094115225aa169bb50674
Kristoffer <kristoffer.ohlund@icloud.com>
2025-05-29 15:04:17 +0200

    Add method to drop an item from the backpack

--- src/Adventure/BackPack.php
+++ src/Adventure/BackPack.php
@@ -3,6 +3,7 @@
 namespace App\Adventure;
 
 use App\Adventure\Item;
+use Exception;
 
 class BackPack
 {
@@ -12,12 +13,36 @@ class BackPack
         $this->items[] = $newItem;
     }
 
+    /**
+     * Drop Item item from backpack
+     * @param Item $dropItem - item to be dropped
+     */
+    public function dropItem(Item $dropItem)
+    {
+        foreach($this->items as $index => $item) {
+            if ($item->getName() === $dropItem->getName()) {
+                unset($this->items[$index]);
+                $this->items = array_values($this->items);
+                return;
+            }
+        }
+        $name = $dropItem->getName();
+        throw new Exception("There is not food with name: $name");
+    }
+
+    /**
+     * Return all items in backpack
+     * 
+     * @return array of items in backpack
+     */
     public function getItems(): ?array {
         return $this->items;
     }
 
     /**
      * Return the item by name, or null if it doesnt exists
+     * @param string $itemName the name of the item to get
+     * @return Item|null
      */
     public function getItemByName(string $itemName): ?Item {
         foreach($this->items as $item) {
