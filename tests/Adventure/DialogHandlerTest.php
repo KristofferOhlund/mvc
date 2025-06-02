f91f5a6294d08b1e2ca6a8c621353b785f6e88c4
Kristoffer <kristoffer.ohlund@icloud.com>
2025-06-02 10:05:52 +0200

    Add tests for DialogHandler

---
+++ tests/Adventure/DialogHandlerTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Adventure;
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\MockObject\MockObject;
+
+class TestDialogHandler extends TestCase
+{   
+    /**
+     * Create instance of Human
+     */
+    public function testDialogInstance() {
+        $dialog = new DialogHandler();
+        $this->assertInstanceOf(DialogHandler::class, $dialog);
+    }
+
+    /**
+     * Test get dialog by status
+     */
+    public function testGetDialogByStatus()
+    {
+        $dialog = new DialogHandler();
+        $dialog->setCurrentItem("Shovel");
+        $dialog->setCurrentRoom("graveyard");
+        $msg = "A rusty shovel, maybe i can dig up some secrets..";
+        $this->assertSame($msg, $dialog->getDialogByStatus());
+    }
+
+    /**
+     * Test get dialog by status
+     */
+    public function testGetCurrentItem()
+    {
+        $dialog = new DialogHandler();
+        $dialog->setCurrentItem("Shovel");
+        $this->assertSame("Shovel", $dialog->getCurrentItem());
+    }
+
+    /**
+     * Test get dialog by status
+     */
+    public function testGetCurrentRoom()
+    {
+        $dialog = new DialogHandler();
+        $dialog->setCurrentRoom("graveyard");
+        $this->assertSame("graveyard", $dialog->getCurrentRoom());
+    }
+}
