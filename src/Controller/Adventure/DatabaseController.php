58434e2b1f7208cdb3c62e89beeaec54e4393a73
Kristoffer <kristoffer.ohlund@icloud.com>
2025-06-03 12:13:28 +0200

    New DB controller

---
+++ src/Controller/Adventure/DatabaseController.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * Module for working with the database
+ */
+
+namespace App\Controller\Adventure;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\Room;
+use App\Entity\Weapon;
+use App\Entity\Food;
+use App\Entity\Tool;
+
+class DatabaseController extends AbstractController
+{   
+    /**
+     * Show all objets and tables in database
+     * @return Response
+     */
+    #[Route("/proj/about/database", name: "index_database")]
+    public function database(): Response
+    {   
+        return $this->render("adventure/database.html.twig");
+    }
+
+
+    /**
+     * CAUTION!
+     * Delete entire database
+     */
+    #[Route("/proj/about/database/delete", name: "delete_database")]
+    public function databaseDelete(EntityManagerInterface $entityManager): Response
+    {   
+        $rooms = $entityManager->getRepository(Room::class)->findAll();
+        foreach($rooms as $room) {
+            $delete = $entityManager->getRepository(Room::class)->find($room);
+            $entityManager->remove($delete);
+            $entityManager->flush();
+        }
+
+
+        $weapons = $entityManager->getRepository(Weapon::class)->findAll();
+        foreach($weapons as $wep) {
+            $delete = $entityManager->getRepository(Weapon::class)->find($wep);
+            $entityManager->remove($delete);
+            $entityManager->flush();
+        }
+        return new Response("Database has been reset");
+    }
+
+
+    /**
+     * Show weapon objects in database
+     * @return Response
+     */
+    #[Route("/proj/about/database/weapons", name:"show_weapons")]
+    public function showWeapons(EntityManagerInterface $entityManager): Response
+    {
+        $weapons = $entityManager->getRepository(Weapon::class)->findAll();
+        return $this->json($weapons);
+    }
+
+
+    /**
+     * Show room objects in database
+     * @return Response
+     */
+    #[Route("/proj/about/database/rooms", name:"show_rooms")]
+    public function showRooms(EntityManagerInterface $entityManager): Response
+    {
+        $rooms = $entityManager->getRepository(Room::class)->findAll();
+        return $this->json($rooms);
+    }
+
+
+    /**
+     * Show food objects in database
+     * @return Response
+     */
+    #[Route("/proj/about/database/foods", name:"show_foods")]
+    public function showFoods(EntityManagerInterface $entityManager): Response
+    {
+        $foods = $entityManager->getRepository(Food::class)->findAll();
+        return $this->json($foods);
+    }
+
+
+    /**
+     * Show tool objects in database
+     * @return Response
+     */
+    #[Route("/proj/about/database/tools", name:"show_tools")]
+    public function showTools(EntityManagerInterface $entityManager): Response
+    {
+        $tools = $entityManager->getRepository(Tool::class)->findAll();
+        return $this->json($tools);
+    }
+}
